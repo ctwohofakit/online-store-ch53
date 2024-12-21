61158c99989644e2e1ec482d47e38ff710fce77b
ctwohofakit <ctwohofa@gmail.com>
2024-12-21 15:02:41 -0800

    Class 01

---
+++ src/components/footer.jsx
@@ -0,0 +1,19 @@
+import "./styles/footer.css";
+
+function Footermenu(){
+    return(
+        <div>
+            <nav>
+                <div className="footer-menu">
+                    <a href="">Log In</a>
+                    <a href="">Search</a>
+                    <a href="">Subscribe</a>
+                    <a href="">Menu</a>
+                </div>
+            </nav>
+        </div>
+
+    );
+}
+
+export default Footermenu;
